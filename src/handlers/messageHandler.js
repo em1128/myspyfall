67e436df5dce9d0632ed1097b9e3037073d0bb34
HomePC <poseidon_x@naver.com>
2025-02-11 23:38:43 +0900

    :bug: Fix: Json 파싱이 실패할 때 예외처리를 위해 try-catch문 적용

--- src/handlers/messageHandler.js
+++ src/handlers/messageHandler.js
@@ -1,31 +1,37 @@
 const roomManager = require('../roomManager');
 
 function handleMessage(ws, message) {
-    const data = JSON.parse(message);
-
-    if (data.type === 'createRoom') {
-        const roomId = roomManager.createRoom(ws);
-        ws.send(JSON.stringify({ type: 'roomCreated', roomId }));
-        console.log(`🔥 방 생성: ${roomId}`);
-    }
-
-    if (data.type === 'joinRoom') {
-        const room = roomManager.joinRoom(ws, data.roomId);
-        if (room) {
-            ws.send(JSON.stringify({ type: 'roomJoined', roomId: data.roomId }));
-            console.log(`👥 유저 입장: ${data.roomId} (현재 ${room.players.length}명)`);
-        } else {
-            ws.send(JSON.stringify({ type: 'error', message: '방이 존재하지 않습니다.' }));
+    try{
+        const data = JSON.parse(message);
+        console.log('roomManager.rooms: ', roomManager.rooms);
+        if (data.type === 'createRoom') {
+            const roomId = roomManager.createRoom(ws);
+            ws.send(JSON.stringify({ type: 'roomCreated', roomId }));
+            console.log(`🔥 방 생성: ${roomId}`);
         }
-    }
 
-    if (data.type === 'ready') {
-        if (roomManager.markReady(data.roomId)) {
-            const room = roomManager.rooms.get(data.roomId);
-            room.players.forEach(player => player.send(JSON.stringify({ type: 'gameStart' })));
-            console.log(`🚀 게임 시작! 방: ${data.roomId}`);
+        if (data.type === 'joinRoom') {
+            const room = roomManager.joinRoom(ws, data.roomId);
+            if (room) {
+                ws.send(JSON.stringify({ type: 'roomJoined', roomId: data.roomId }));
+                console.log(`👥 유저 입장: ${data.roomId} (현재 ${room.players.length}명)`);
+            } else {
+                ws.send(JSON.stringify({ type: 'error', message: '방이 존재하지 않습니다.' }));
+            }
         }
+
+        if (data.type === 'ready') {
+            if (roomManager.markReady(data.roomId)) {
+                const room = roomManager.rooms.get(data.roomId);
+                room.players.forEach(player => player.send(JSON.stringify({ type: 'gameStart' })));
+                console.log(`🚀 게임 시작! 방: ${data.roomId}`);
+            }
+        }
+    }catch(e){
+        console.error('메시지가 올바르지 않습니다.');
+        ws.send(JSON.stringify({ type: 'error', message: `메시지 형식이 올바르지 않습니다.(${message}` }));
     }
+    
 }
 
 module.exports = handleMessage;
